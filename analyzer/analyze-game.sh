6c1835e2fb1aa74c7f5648aebfb916e72a7df292
Nathan Jhaveri <jhaveri@umich.edu>
2015-03-09 21:18:59 -0700

    Remove outdated shell script

--- analyzer/analyze-game.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /var/www/chessanalyze/
-python analyze.py --email --thinktime=7000 --pgnextract=/var/www/chessanalyze/external/pgn-extract/pgn-extract --stockfish=/var/www/chessanalyze/external/stockfish/src/stockfish
-
--- analyzer/makegame.sh
+++
@@ -1,2 +0,0 @@
-python analyze.py --outfile=output.json --thinktime=2 pgn-files/nnnnnnnn-vs-Gogogorosh.2014.04.11.pgn
-echo "define(function(){var game=" > web/js/game.js; cat output.json | python -mjson.tool >> web/js/game.js; echo "; return game;});" >> web/js/game.js
